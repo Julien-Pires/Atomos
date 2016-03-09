e12937253269721960b77d8a02873e10ee3669c3
Julien Pires <jpires@ohbibi.com>
2016-03-10 00:32:11 +0100

    Add Pool class & tests

---
+++ src/src/Atomos/Atomos/Pool.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Atomos.Atomos
+{
+    public class Pool<T> : IDisposable where T : class
+    {
+        #region Properties
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Pool()
+        {
+        }
+
+        public Pool(int capacity)
+        {
+        }
+
+        public Pool(Func<T> createAction, Action<T> resetAction)
+        {
+        }
+
+        public Pool(int capacity, Func<T> createAction, Action<T> resetAction)
+        {
+        }
+
+        #endregion
+
+        #region Cleanup
+
+        public void Reset(bool cleanItems = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Pooling
+
+        public T Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/tests/Atomos.Tests/PoolTest.cs
@@ -0,0 +1,47 @@
+﻿using Atomos.Atomos;
+
+using Xunit;
+
+namespace Atomos.Tests
+{
+    public class PoolTest
+    {
+        #region Nested
+
+        private class PoolItemTest
+        {
+            #region Properties
+
+            public int Value { get; set; }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Fields
+
+        private Pool<PoolItemTest> _pool;
+
+        #endregion
+
+        #region Constructors
+
+        public PoolTest()
+        {
+            _pool = new Pool<PoolItemTest>();
+        }
+
+        #endregion
+
+        #region Pooling Tests
+
+        [Fact]
+        public void Pool_Get_Success()
+        {
+            Assert.NotNull(_pool.Get());
+        }
+
+        #endregion
+    }
+}
