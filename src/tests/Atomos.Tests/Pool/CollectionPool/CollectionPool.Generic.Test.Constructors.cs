7d31229d9719e5b0226669b282661cc736719c4e
Julien Pires <piresjulien95@gmail.com>
2016-04-03 01:32:08 +0200

    Add collection pool tests

---
+++ src/tests/Atomos.Tests/Pool/CollectionPool/CollectionPool.Generic.Test.Constructors.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Atomos.Tests.Pool
+{
+    public abstract partial class CollectionPool_Generic_Test<TPool, TItem>
+    {
+        [Theory]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
+        public void Ctor_WhenInvalidCollectionPoolMode_ThrowException(int mode)
+        {
+            Assert.Throws<PoolException>(() => Build((CollectionPoolMode)mode));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(int.MinValue)]
+        public void Ctor_WhenNegativeInitialCapacity_ThrowException(int initialCapacity)
+        {
+            Assert.Throws<PoolException>(() => Build(initialCapacity: initialCapacity));
+        }
+    }
+}
